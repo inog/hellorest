dcae5ba67106a7154d41f8e64c10c7a9b8422350
inog <kontakt@ingo-reschke.de>
2019-09-20 15:13:06 +0200

    add get time endpoint

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
@@ -13,5 +15,10 @@ public class GreetingController {
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         return new Greeting((counter.incrementAndGet()), String.format("Hello %s", name));
+    }
+
+    @RequestMapping("/time")
+    public String time() {
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss"));
     }
 }
