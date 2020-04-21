db13065d81d93e89d5143841c9485859ba7ceb41
inog <kontakt@ingo-reschke.de>
2020-04-22 00:21:24 +0200

    add crud methods.. with Mappings to see wether it works

--- src/main/java/app/user/UserController.java
+++ src/main/java/app/user/UserController.java
@@ -1,10 +1,28 @@
 package app.user;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("users") // http://localhost:8080/users
 public class UserController {
 
+    @GetMapping
+    public String getUser(){
+        return "getUser was called.";
+    }
+
+    @PostMapping
+    public String createUser(){
+        return "createUser was called";
+    }
+
+    @PutMapping
+    public String updateUser(){
+        return "updateUser was called";
+    }
+
+    @DeleteMapping
+    public String deleteUser(){
+        return "deleteUser was called";
+    }
 }
