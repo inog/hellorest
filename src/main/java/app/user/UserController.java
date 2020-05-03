f96574d3168caac3a95d1ccd95b11a57d4a7e95a
inog <kontakt@ingo-reschke.de>
2020-05-04 00:00:38 +0200

    deliver xml format.

--- src/main/java/app/user/UserController.java
+++ src/main/java/app/user/UserController.java
@@ -1,5 +1,6 @@
 package app.user;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -7,35 +8,35 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     @GetMapping()
-    public String getUsers(@RequestParam(value="page", defaultValue = "1") int page,
-                           @RequestParam(value="limit", defaultValue = "50") int limit,
-                           @RequestParam(value="sort", defaultValue = "asc", required = false) String sort){
+    public String getUsers(@RequestParam(value = "page", defaultValue = "1") int page,
+                           @RequestParam(value = "limit", defaultValue = "50") int limit,
+                           @RequestParam(value = "sort", defaultValue = "asc", required = false) String sort) {
         return "getUser was called with page = " + page + " limtt = " + limit + " sort = " + sort;
     }
 
-    @GetMapping(path = "/{userId}")
-    public UserRest getUser(@PathVariable String userId){
+    @GetMapping(path = "/{userId}", produces = MediaType.APPLICATION_XML_VALUE)
+    public UserRest getUser(@PathVariable String userId) {
         UserRest user = UserRest.builder()
                 .firstName("Ingo")
                 .lastName("Test")
                 .email("test@test.de")
                 .userId("007")
                 .build();
-         return user;
+        return user;
     }
 
     @PostMapping
-    public String createUser(){
+    public String createUser() {
         return "createUser was called";
     }
 
     @PutMapping
-    public String updateUser(){
+    public String updateUser() {
         return "updateUser was called";
     }
 
     @DeleteMapping(path = "/{userId}")
-    public String deleteUser(@PathVariable String userId){
+    public String deleteUser(@PathVariable String userId) {
         return "deleteUser with id = " + userId + " was called";
     }
 }
